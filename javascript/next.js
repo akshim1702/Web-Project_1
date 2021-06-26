4afe1f2694b75cd01bed376f343e22a588fc4ebc
aparnakatiyar2 <85848425+aparnakatiyar2@users.noreply.github.com>
2021-06-26 18:48:07 +0530

    Update next.js

--- javascript/next.js
+++ javascript/next.js
@@ -1,10 +1,15 @@
-// Function for taking input and printing rows 
+//taking name and displaying on top
 
+// let space = " ";
+// let n = prompt('Enter your name', 'enter full name') + space;
+// let position = n.slice(0, n.indexOf(' '));
+
+// document.getElementById('head').innerHTML = "<h1>" + position + "'s" + " Time Table" + "</h1>";
 
 function TakeInput() {
-    let input = document.getElementById('Columns').value;
-    let table = document.getElementById("myTable");
 
+    let input = document.getElementById('Row').value;
+    var table = document.getElementById('myTable');
     if (input <= 20) {
         for (let i = 0; i < input; i++) {
 
@@ -14,7 +19,11 @@ function TakeInput() {
 
                 let cell = new Array(index);
                 cell = row.insertCell(index);
-                cell.innerHTML = '<td><input type="text" name = ""></td>';
+                if (index == 0) {
+                    cell.innerHTML = '<td><input type="time"  class="time-period1" name = ""></td>' + '<td><input type="time" class="time-period2" name = ""></td>';
+                } else {
+                    cell.innerHTML = '<td><input type="text" name = ""></td>';
+                }
 
             }
         }
@@ -23,6 +32,19 @@ function TakeInput() {
         alert("cannot add rows more than 20 on a single count");
     }
 }
+
+//deletes rows according to no. mentioned
+
+function deleteRow() {
+
+    let input = document.getElementById('deleter').value;
+    var table = document.getElementById('myTable');
+    for (let index = 0; index < input; index++) {
+        let row = table.deleteRow(index);
+
+    }
+}
+
 // inserting a single row 
 
 function add() {
@@ -32,7 +54,12 @@ function add() {
 
         let cell = new Array(index);
         cell = row.insertCell(index);
-        cell.innerHTML = '<td><input type="text" name = ""></td>';
+        if (index == 0) {
+            cell.innerHTML = '<td><input type="time" class="time-period1" name = ""></td>' + '<td><input type="time" class="time-period2" name = ""></td>';
+
+        } else {
+            cell.innerHTML = '<td><input type="text" name = ""></td>';
+        }
 
     }
 }
@@ -44,25 +71,28 @@ function delete5() {
     var table = document.getElementById('myTable');
     var rowCount = table.rows.length;
     console.log(rowCount);
-    let i = 0;
-    if (i == 0) {
-        table.deleteRow(0);
+    let i = 1;
+    if (i == 1) {
+        table.deleteRow(1);
         i++;
 
     }
 }
-
 // Deleting all the rows
 
 function delete6() {
     var table = document.getElementById('myTable');
     var rowCount = table.rows.length;
 
-    confirm("Are you sure? You are going to delete all the rows");
+    confirm("Are you sure? You want to delete all the rows");
     for (var i = 0; i < rowCount; i++) {
-        var row = table.rows[i];
-        table.deleteRow(i);
-        rowCount--;
-        i--;
+        if (i == 0) {
+            alert("Last row cannot be deleted");
+        } else {
+            var row = table.rows[i];
+            table.deleteRow(i);
+            rowCount--;
+            i--;
+        }
     }
 }
