3cdf769fef9a2384b97dedde893773637fc0562a
akshim1702 <73992286+akshim1702@users.noreply.github.com>
2021-06-27 19:35:55 +0530

    Update next.js

--- javascript/next.js
+++ javascript/next.js
@@ -1,10 +1,12 @@
 //taking name and displaying on top
 
- let space = " ";
- let n = prompt('Enter your name', 'enter full name') + space;
- let position = n.slice(0, n.indexOf(' '));
+let space = " ";
+let n = prompt('Enter your name', 'enter full name') + space;
+let position = n.slice(0, n.indexOf(' '));
 
- document.getElementById('head').innerHTML = "<h1>" + position + "'s" + " Time Table" + "</h1>";
+document.getElementById('head').innerHTML = "<h1 id= 'font'>" + position + "'s" + " Time Table" + "</h1>";
+
+// Taking input by entering value 
 
 function TakeInput() {
 
@@ -22,7 +24,7 @@ function TakeInput() {
                 if (index == 0) {
                     cell.innerHTML = '<td><input type="time"  class="time-period1" name = ""></td>' + '<td><input type="time" class="time-period2" name = ""></td>';
                 } else {
-                    cell.innerHTML = '<td><input type="text" name = ""></td>';
+                    cell.innerHTML = '<td><textarea name="" class="text" cols="24" rows="1"></textarea></td>';
                 }
 
             }
@@ -31,6 +33,7 @@ function TakeInput() {
     } else {
         alert("cannot add rows more than 20 on a single count");
     }
+
 }
 
 //deletes rows according to no. mentioned
@@ -39,10 +42,25 @@ function deleteRow() {
 
     let input = document.getElementById('deleter').value;
     var table = document.getElementById('myTable');
-    for (let index = 0; index < input; index++) {
-        let row = table.deleteRow(index);
+
+    var rowCount = table.rows.length;
+
+
+    if (input > rowCount) {
+        alert("inputted value does not exist");
+    } else {
+        for (let index = 0; index < input; index++) {
+            if (input == 1) {
+                alert("last row can't be deleted");
+            } else {
+
+                let row = table.deleteRow(index);
+            }
+
+        }
 
     }
+
 }
 
 // inserting a single row 
@@ -58,7 +76,7 @@ function add() {
             cell.innerHTML = '<td><input type="time" class="time-period1" name = ""></td>' + '<td><input type="time" class="time-period2" name = ""></td>';
 
         } else {
-            cell.innerHTML = '<td><input type="text" name = ""></td>';
+            cell.innerHTML = '<td><textarea name="" class="text" cols="24" rows="1"></textarea></td>';
         }
 
     }
